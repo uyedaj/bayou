8f561479fbd7d0b23568173af81fc6277831bf51
uyedaj <juyeda@vt.edu>
2018-05-28 14:17:52 -0400

    Fixed tests

--- inst/tests/test-weight_matrix.r
+++ inst/tests/test-weight_matrix.r
@@ -1,21 +1,15 @@
 context("weight matrix can be calculated")
 test_that("weight matrix can be calculated", {
-  data(chelonia.simmap)
-  tree <- chelonia.simmap$tree
-  dat <- chelonia.simmap$dat
-  emap <- chelonia.simmap$emap
+  data(chelonia)
+  tree <- chelonia$phy
+  dat <- chelonia$dat
   cache <- .prepare.ou.univariate(tree, dat)
-  pars <- list(alpha=0.1, sig2=1, k=16, optima=c(3,4,5,6), ntheta=4)
+  pars <- list(alpha=0.01, sig2=1, k=3, theta=c(3,4,5), ntheta=3, sb=c(411, 400, 47), loc=c(25, 17, 33), t2=c(2,3,3))
   TotExp <- exp(-cache$height*pars$alpha)
-  expect_that(apply(simmap.W(tree, pars),1,sum),equals(rep(1,226)))
-  expect_that(apply(simmap.W(cache, pars),1,sum),equals(rep(1,226)))
-  expect_that(apply(.simmap.W(cache,pars),1,sum),equals(rep(1,226)))
-  expect_that(apply(edgemap.W(tree,pars,emap),1,sum),equals(rep(1,226)))
-  expect_that(apply(.edgemap.W(cache,pars,emap,TotExp),1,sum),equals(rep(1,226)))
-  expect_that(simmap.W(tree,pars),equals(edgemap.W(tree,pars,emap)))
-  pars$sb <- which(emap$sh==1)
-  pars$loc <- emap$r1[emap$sh==1]
-  pars$t2 <- emap$t2[emap$sh==1]
-  expect_that(.parmap.W(cache,pars), equals(edgemap.W(tree,pars,emap)))
-  expect_that(parmap.W(tree,pars), equals(edgemap.W(tree,pars,emap)))
+  stree <- pars2simmap(pars, tree)
+  sW <- simmap.W(stree$tree, pars)
+  expect_that(apply(sW, 1, sum), equals(rep(1,226)))
+  bW <- bayou:::C_weightmatrix(cache, pars)$W
+  expect_that(bW, equals(sW))
+  expect_that(apply(bW,2,sum), equals(c(79.58365, 117.46516, 28.95119), tolerance=0.0001))
 })
