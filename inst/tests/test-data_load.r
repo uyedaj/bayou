7562228deb8d3fe52e6eaed8fac5c4925cc0e24c
uyedaj <juyeda@vt.edu>
2020-09-16 15:16:04 -0400

    updated test functions

--- inst/tests/test-data_load.r
+++ inst/tests/test-data_load.r
@@ -1,5 +1,5 @@
 #context("data can be loaded")
 testthat::test_that("data can be loaded", {
-  data(chelonia)
+  data(chelonia, package="geiger")
   testthat::expect_equal(length(chelonia$phy$tip.label),226)
 })
